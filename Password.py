cf73fcc1948b4341067456a8dcd3ae6ad22aac51
Devesh Goyal <93080269+deveshgoyal1000@users.noreply.github.com>
2022-06-23 21:23:09 +0530

    Delete Password.py

--- Password.py
+++
@@ -1,6 +0,0 @@
-import random
-
-passlen = int(input("Enter the length of password"))
-s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-p = "".join(random.sample(s,passlen))
-print(p)
